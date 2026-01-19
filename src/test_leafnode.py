77123ef1d43607328b1686104a1e9ca6c58784b5
Paul Pastor <paulpastor@me.com>
2026-01-19 13:58:52 -0500

    fixed index

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -14,25 +14,25 @@ class TestLeafNode(unittest.TestCase):
         node2 = LeafNode("p", "This is a different paragraph", {"class": "text"})
         self.assertNotEqual(node, node2)
 
-        def test_leaf_to_html_p(self):
-            node = LeafNode("p", "Hello, world!")
-            self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
-        def test_leaf_to_html_with_props(self):
-            node = LeafNode("a", "Click me", {"href": "https://example.com"})
-            self.assertEqual(
-                node.to_html(),
-                '<a href="https://example.com">Click me</a>',
-            )
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("a", "Click me", {"href": "https://example.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://example.com">Click me</a>',
+        )
 
-        def test_leaf_to_html_no_tag(self):
-            node = LeafNode(None, "Just text")
-            self.assertEqual(node.to_html(), "Just text")
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(node.to_html(), "Just text")
 
-        def test_leaf_to_html_raises_on_empty_value(self):
-            node = LeafNode("p", None)
-            with self.assertRaises(ValueError):
-                node.to_html()
+    def test_leaf_to_html_raises_on_empty_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
 
 
 if __name__ == "__main__":
