43ec3b3f2801026c683dd1c1e76ed82aad3d7921
Paul Pastor <paulpastor@me.com>
2026-01-18 21:38:10 -0500

    add initial implementation of main script and text node classes

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    tn = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(tn)
+
+
+if __name__ == "__main__":
+    main()
