88d3c10b00c45947b5408fed91504c3fb6d41218
Paul Pastor <paulpastor@me.com>
2026-01-20 20:25:40 -0500

    remove unecessary __main__

--- src/utils.py
+++ src/utils.py
@@ -125,12 +125,3 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(curr_text, TextType.TEXT))
 
     return new_nodes
-
-
-if __name__ == "__main__":
-    node = TextNode(
-        "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
-        TextType.TEXT,
-    )
-    new_nodes = split_nodes_link([node])
-    print(new_nodes)
