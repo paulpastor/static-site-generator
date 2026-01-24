0a6ba12cc62a8b3131c379c4bd5fb811f9d2f288
Paul Pastor <paulpastor@me.com>
2026-01-23 20:20:29 -0500

    move files from static to public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def move_recursive(src_dir, dest_dir):
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+
+    os.mkdir(dest_dir)
+
+    items = os.listdir(src_dir)
+
+    for item in items:
+        src_path = os.path.join(src_dir, item)
+        dest_path = os.path.join(dest_dir, item)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+        else:
+            move_recursive(src_path, dest_path)
 
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    move_recursive("static", "public")
 
 
 main()
